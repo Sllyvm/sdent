3c810914137c6fbfc20b802d159e5c80038cc0a8
alex <2259028043@qq.com>
2022-09-20 18:44:53 +0800

    revert(版本优化): setrr

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,37 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 项目打包出现白屏
+  // publicPath:'./',
+  devServer: {
+    port: 8888,
+    host: 'localhost',
+    open: true,
+    https: false,
+    proxy: {
+      // /dev-api 代理名称 process.env.VUE_APP_BASE_API === "/dev-api"
+      // [process.env.VUE_APP_BASE_API] : {
+      //   target: process.env.VUE_APP_SERVICE_URL,
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     ["^" + process.env.VUE_APP_BASE_API]: ""
+      //   }
+      // }
+      [process.env.VUE_APP_BASE_API] : {
+        target : "http://localhost:3000",
+        changeOrigin : true,
+        pathRewrite : {
+          ["^" + process.env.VUE_APP_BASE_API] : ''
+        }
+      }
+      // "/dev-api":{
+      //   target:"http://localhost:3000",
+      //   changeOrigin:true,
+      //   pathRewrite:{
+      //     "^/dev-api":''
+      //   }
+      // }
+    }
+  },
+  lintOnSave: false
 });
